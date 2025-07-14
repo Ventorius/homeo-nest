3ac92107bb34277fbada0fbd18cc7e1693729047
sebastianbasznianin <avaven@gmail.com>
2025-07-14 19:58:33 +0200

    feat: change port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
@@ -19,7 +21,7 @@ async function bootstrap() {
     publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
   });
 
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
